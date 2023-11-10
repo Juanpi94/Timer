60946ed48ca9a1daee58650ca7a02419f30a9be1
Juanpi94 <juang.94.ucr@gmail.com>
2023-11-10 16:32:34 -0600

    Tercer commit

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            
+            isBlock: false,
         }
     },
     mounted: function () {
 
     },
     methods: {
-
+        cambiarValor() {
+            this.isBlock = !this.isBlock;
+          }
     },
 });
